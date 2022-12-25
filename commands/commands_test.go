da3d99f446d4d089d58bf274fe1937f3f3cc9341
Jonathan A <me@iotku.pw>
2022-12-25 12:25:13 -0800

    Remove truth checks for removed parameters in test for getCommandAndArg

--- commands/commands_test.go
+++ commands/commands_test.go
@@ -13,51 +13,47 @@ func init() {
 }
 
 func TestGetCommandAndArg(t *testing.T) {
-	trueFalse := [2]bool{true, false}
-	for _, truth := range trueFalse {
-		// message with nickname as prefix
-		got1, got2 := getCommandAndArg("MusicButt command argument", "MusicButt", truth, &Config)
-		if got1 != "command" || got2 != "argument" {
-			t.Errorf("got %q %q  wanted %q %q, truth %t", got1, got2, "command", "argument", truth)
-		}
-
-		got1, got2 = getCommandAndArg("MusicButt command", "MusicButt", truth, &Config)
-		if got1 != "command" || got2 != "" {
-			t.Errorf("got %q %q, wanted %q %q, truth %t", got1, got2, "command", "", truth)
-		}
-
-		got1, got2 = getCommandAndArg("MusicButt", "MusicButt", truth, &Config)
-		if got1 != "" || got2 != "" {
-			t.Errorf("got %q %q, wanted %q %q, truth: %t", got1, got2, "", "", truth)
-		}
-
-		got1, got2 = getCommandAndArg("!command argument", "MusicButt", truth, &Config)
-		if got1 != "command" || got2 != "argument" {
-			t.Errorf("got %q %q, wanted %q %q, truth: %t", got1, got2, "command", "argument", truth)
-		}
-		got1, got2 = getCommandAndArg("!command", "MusicButt", truth, &Config)
-		if got1 != "command" || got2 != "" {
-			t.Errorf("got %q %q, wanted %q %q, truth: %t", got1, got2, "command", "", truth)
-		}
-
-		got1, got2 = getCommandAndArg("!", "MusicButt", truth, &Config)
-		if got1 != "" || got2 != "" {
-			t.Errorf("got %q %q, wanted %q %q, truth: %t", got1, got2, "", "", truth)
-		}
-
-	}
-
-	got1, got2 := getCommandAndArg("command argument", "MusicButt", true, &Config)
+	// message with nickname as prefix
+	got1, got2 := getCommandAndArg("MusicButt command argument", "MusicButt", &Config)
 	if got1 != "command" || got2 != "argument" {
-		t.Errorf("got %q %q, wanted %q %q", got1, got2, "command", "argument")
+		t.Errorf("got %q %q  wanted %q %q", got1, got2, "command", "argument")
 	}
 
-	got1, got2 = getCommandAndArg("command", "MusicButt", true, &Config)
+	got1, got2 = getCommandAndArg("MusicButt command", "MusicButt", &Config)
 	if got1 != "command" || got2 != "" {
 		t.Errorf("got %q %q, wanted %q %q", got1, got2, "command", "")
 	}
 
-	got1, got2 = getCommandAndArg("", "MusicButt", true, &Config)
+	got1, got2 = getCommandAndArg("MusicButt", "MusicButt", &Config)
+	if got1 != "" || got2 != "" {
+		t.Errorf("got %q %q, wanted %q %q", got1, got2, "", "")
+	}
+
+	got1, got2 = getCommandAndArg("!command argument", "MusicButt", &Config)
+	if got1 != "command" || got2 != "argument" {
+		t.Errorf("got %q %q, wanted %q %q", got1, got2, "command", "argument")
+	}
+	got1, got2 = getCommandAndArg("!command", "MusicButt", &Config)
+	if got1 != "command" || got2 != "" {
+		t.Errorf("got %q %q, wanted %q %q", got1, got2, "command", "")
+	}
+
+	got1, got2 = getCommandAndArg("!", "MusicButt", &Config)
+	if got1 != "" || got2 != "" {
+		t.Errorf("got %q %q, wanted %q %q", got1, got2, "", "")
+	}
+
+	got1, got2 = getCommandAndArg("command argument", "MusicButt", &Config)
+	if got1 != "command" || got2 != "argument" {
+		t.Errorf("got %q %q, wanted %q %q", got1, got2, "command", "argument")
+	}
+
+	got1, got2 = getCommandAndArg("command", "MusicButt", &Config)
+	if got1 != "command" || got2 != "" {
+		t.Errorf("got %q %q, wanted %q %q", got1, got2, "command", "")
+	}
+
+	got1, got2 = getCommandAndArg("", "MusicButt", &Config)
 	if got1 != "" || got2 != "" {
 		t.Errorf("got %q %q, wanted %q %q", got1, got2, "", "")
 	}
