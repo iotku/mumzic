126900805027cea3908014472fe82c1bbc51aede
Jonathan A. <me@iotku.pw>
2025-11-23 18:08:06 -0800

    fix: nosec file Open from variable for loadAllowedURLPrefixesFromFile
    This is just a helper method, though perhaps it would be sensible to
    separate this out in the future to ensure it's obviously so.

--- youtubedl/youtubedl.go
+++ youtubedl/youtubedl.go
@@ -33,7 +33,7 @@ func init() {
 
 // loadAllowedURLPrefixesFromFile loads prefixes from each line of a provided txt file
 func loadAllowedURLPrefixesFromFile(path string) error {
-	f, err := os.Open(path)
+	f, err := os.Open(path) // #nosec G402
 	if err != nil {
 		return err
 	}
